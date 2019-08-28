91e288f2308b555e410234ef7c8d55752e30432a
evertondg <evertondg@hotmail.com.br>
2019-08-27 22:51:39 -0300

    Aplicação MeetApp [login e validacoes de login ]

---
+++ meetapp/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'b9db7c18c596e7ce905f38031157e953',
+  expiresIn: '7d',
+};
--- meetapp/src/routes.js
+++ meetapp/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionsController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello2 World' });
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionsController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
