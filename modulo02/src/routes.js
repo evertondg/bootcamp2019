bcf0d8e80a151ce85d6ad30907ab62278f752539
evertondg <evertondg@hotmail.com.br>
2019-08-28 22:30:43 -0300

    feat: Adicionado uploads e Relacionamento entre Usuários e Avatar

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,16 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
+// Middlewares
 import authMiddleware from './app/middlewares/auth';
+
 // Controllers
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+// Abaixo  desta linha todas as rotas necessitam de token de autenticação
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
