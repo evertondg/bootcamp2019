2eeb9387a39ace8fb542258aefbe2deb081c8264
evertondg <evertondg@hotmail.com.br>
2019-08-29 21:44:44 -0300

    feat: Criado rota para servir os prestadores de serviço do app GoBarber

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -9,6 +9,7 @@ import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -21,5 +22,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 
 export default routes;
