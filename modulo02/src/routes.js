3b86256f5faea3ed40aecafd60643a6c45c07658
evertondg <evertondg@hotmail.com.br>
2019-08-29 22:53:34 -0300

    feat: Criado Rota que cria os agendamentos do usuário para o prestador de serviço

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -10,6 +10,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -21,8 +22,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), FileController.store);
-
 routes.get('/providers', ProviderController.index);
 
+routes.post('/appointments', AppointmentController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
