c1d358b0da2982b52fe4e6f5040413d507654c4d
evertondg <evertondg@hotmail.com.br>
2019-08-31 15:03:57 -0300

    feat: Adicionado e Configurado Mongoose para conectar no mongoDB

--- modulo02/src/database/index.js
+++ modulo02/src/database/index.js
@@ -1,5 +1,6 @@
 import Sequelize from 'sequelize';
 
+import { mongoose } from 'mongoose';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
@@ -19,6 +20,16 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+      }
+    );
   }
 }
 
