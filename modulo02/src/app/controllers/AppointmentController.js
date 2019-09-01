a78b82be32acd89abdcbda29eae1030c9206b553
evertondg <evertondg@hotmail.com.br>
2019-09-01 15:51:12 -0300

    feat: Comunicado para o prestador de serviço  via email (nodemailer) quando o usuário cancelar o agendamento

--- modulo02/src/app/controllers/AppointmentController.js
+++ modulo02/src/app/controllers/AppointmentController.js
@@ -6,8 +6,9 @@ import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
-class AppointmentController {q
+class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
     const appointments = await Appointment.findAll({
@@ -130,7 +131,15 @@ class AppointmentController {q
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -139,13 +148,21 @@ class AppointmentController {q
     }
 
     const dateWithSub = subHours(appointment.date, 2);
-    if(isBefore(dateWithSub,new Date())){
-      return res.status(401).json({error: 'You can only cancel appointments 2 hours in advance.'})
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointments 2 hours in advance.',
+      });
     }
 
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem um novo cancelamento',
+    });
 
     return res.json(appointment);
   }
---
+++ modulo02/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '66e7c9d006dd5f',
+    pass: 'a332a3fa909b49',
+  },
+  default: {
+    from: ' Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
